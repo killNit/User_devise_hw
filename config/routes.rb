dec1934c44842b6c53cebf9d97ec6390edaacc12
killNit <rileysanti@gmail.com>
2020-08-31 14:46:19 -0600

    log_out_works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-# root "sports#index"
+  root "sports#index"
   devise_for :users
 
   resources :sports
---
+++ db/migrate/20200831202359_add_users_to_sports.rb
@@ -0,0 +1,5 @@
+class AddUsersToSports < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :sports, :user, null: false, foreign_key: true
+  end
+end
